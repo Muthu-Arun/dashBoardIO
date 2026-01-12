1657a50a21f8437ad9d8be47b29877aaae9d90b7
Muthu-Arun <amuthu165@gmail.com>
2026-01-12 23:23:32 +0530

    parsejson struture defined - work in progress

--- src/parsenrender/json.h
+++ src/parsenrender/json.h
@@ -1,4 +1,16 @@
+#include "widget.h"
+#include <memory>
+#include <string>
+#include <unordered_map>
+namespace ParseJson {
 
-namespace Parse {
+    struct DataStore{
+        std::unordered_map<std::string, std::shared_ptr<int>> MapInt;
+        std::unordered_map<std::string, std::shared_ptr<float>> MapFloat;
+        std::unordered_map<std::string, std::shared_ptr<std::string>> MapString;
+    };
+    class HttpWindowWrapper {
+        std::unordered_map<std::string, std::unique_ptr<Widgets::Widget>> widgets;
 
+    };
 }
