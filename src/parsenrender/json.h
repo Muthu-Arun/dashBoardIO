29227bf706dd35e319c633b86cc78b3f70aa5eaa
Muthu-Arun <amuthu165@gmail.com>
2026-01-13 19:51:43 +0530

    formatting style change

--- src/parsenrender/json.h
+++ src/parsenrender/json.h
@@ -2,15 +2,18 @@
 #include <memory>
 #include <string>
 #include <unordered_map>
+#include <vector>
 namespace ParseJson {
 
-    struct DataStore{
-        std::unordered_map<std::string, std::shared_ptr<int>> MapInt;
-        std::unordered_map<std::string, std::shared_ptr<float>> MapFloat;
-        std::unordered_map<std::string, std::shared_ptr<std::string>> MapString;
-    };
-    class HttpWindowWrapper {
-        std::unordered_map<std::string, std::unique_ptr<Widgets::Widget>> widgets;
+class HttpWindowWrapper {
+protected:
+  std::unordered_map<std::string, std::unique_ptr<Widgets::Widget>> widgets;
 
-    };
-}
+  std::unordered_map<std::string, std::shared_ptr<int>> MapInt;
+  std::unordered_map<std::string, std::shared_ptr<float>> MapFloat;
+  std::unordered_map<std::string, std::shared_ptr<std::string>> MapString;
+  std::string host_endpoint;
+
+public:
+};
+} // namespace ParseJson
